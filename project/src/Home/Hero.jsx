f9969740ff050f97e72c724c0acbc1ff1dc6145e
Diana-Jackson <d14415974@gmail.com>
2024-11-08 09:39:00 -0800

    creates the hero section

--- project/src/Home/Hero.jsx
+++ project/src/Home/Hero.jsx
@@ -1,9 +1,16 @@
-
-function Hero (){
+import "./Home.css"
+function Hero  (){
     return(
         <>
-        <div>
-            <h1>hero</h1>
+        <div className="hero-section">
+        <div className="hero-content">
+                <h1 className="tagline">Make your house homey and elegant with comfort home furnitures</h1>
+                <p className="hero-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Impedit, odit. Animi, accusantium. Dolorum, adipisci dolores esse voluptatibus ab quisquam ipsa.</p>
+                <div className="cta">
+                <a href="">view more</a>
+                </div>
+            </div>
+            
         </div>
         </>
     )
