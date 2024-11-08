37a39f65de66e9275edb41e2623748ed57f90ef0
Diana-Jackson <d14415974@gmail.com>
2024-11-08 10:46:45 -0800

    creating the about section

--- project/src/Home/About.jsx
+++ project/src/Home/About.jsx
@@ -1,9 +1,22 @@
-
+import image1 from "../assets/about.jpg"
+import image2 from "../assets/bcg2.jpg"
+import "./Home.css"
 function About (){
     return(
         <>
-        <div>
-            <h1>about</h1>
+        <div className="about-section">
+            
+            <div className="about-text">
+            <h1 className="title">about us</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Esse quod facere pariatur neque consectetur odit inventore error dolore autem omnis voluptatem quo est, voluptatibus quas, quae fuga magnam. Unde, impedit.</p>
+
+            </div>
+            <div className="about-images">
+                <img id="img1" src={image2} alt="" />
+                <img id="img2" src={image1} alt="" />
+
+            </div>
+            
         </div>
         </>
     )
