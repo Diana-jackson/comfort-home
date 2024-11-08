bb658c36b285f83d0a0573dedc30c22471f1b667
Diana-Jackson <d14415974@gmail.com>
2024-11-08 13:12:54 -0800

    sets up the products development environment

--- project/src/Home/Home.jsx
+++ project/src/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Hero from "./Hero"
 import About from "./About"
+import Products from "./Products"
 function Home (){
     return(
         <>
         <Hero/>
         <About/>
+        <Products/>
         </>
     )
 }
---
+++ project/src/Home/Products.jsx
@@ -0,0 +1,11 @@
+import "./Home.css"
+function Products () {
+    return(
+        <>
+        <div>
+        <h1>products</h1>
+        </div>
+        </>
+    )
+}
+export default Products
