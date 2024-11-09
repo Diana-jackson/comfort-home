1dd4dde0db2c7e5d6825a12c7cc11ee689fb1ce3
Diana-Jackson <d14415974@gmail.com>
2024-11-09 03:55:58 -0800

    sets up the testimonies development environment

--- project/src/Home/Home.jsx
+++ project/src/Home/Home.jsx
@@ -1,12 +1,14 @@
 import Hero from "./Hero"
 import About from "./About"
 import Products from "./Products"
+import Testimonies from "./Testimonies"
 function Home (){
     return(
         <>
         <Hero/>
         <About/>
         <Products/>
+        <Testimonies/>
         </>
     )
 }
---
+++ project/src/Home/Testimonies.jsx
@@ -0,0 +1,12 @@
+import "./Home.css"
+
+function Testimonies () {
+    return(
+        <>
+        <div>
+            <h1>testimonies</h1>
+        </div>
+        </>
+    )
+}
+export default Testimonies
